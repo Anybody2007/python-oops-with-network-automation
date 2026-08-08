afb17f62f38aa244a65746c2176322fbde1dad23
Arijit Sarkar <arijetsarkar@gmail.com>
2026-08-08 22:10:53 +0530

    Setup a dedicated logging file

--- 07_multi_vendor_framework/logging_basic_config.py
+++ 07_multi_vendor_framework/logging_basic_config.py
@@ -1,4 +1,4 @@
 import logging
 
 def logging_basic_config():
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, filename="multivendor_fw.log", format="%(asctime)s, %(levelname)s, %(message)s", force=True)
